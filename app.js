6659d09f3a9fc68390881aa0ba171d6934f813a0
Asfak Ahmed <asfakahmed680@gmail.com>
2023-10-07 21:26:21 +0600

    app complete

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 function onSubmit() {
+  const name = document.getElementById("name").value;
+  const number = document.getElementById("number").value;
+  const email = document.getElementById("email").value;
+  const messageBody =
+    "Name:" + name + "<br/> Number: " + number + "<br/> Email: " + email;
   Email.send({
     Host: "smtp.elasticemail.com",
     Username: "asfakahmed580@gmail.com",
     Password: "EAF7A13744E606CC89AEDD39BF5697973E3D",
     To: "asfakahmed680@gmail.com",
     From: "asfakahmed580@gmail.com",
-    Subject: "This is the subject",
-    Body: "And this is the body",
-  }).then((message) => alert(message));
+    Subject: "Someone telling about me",
+    Body: messageBody,
+  }).then((message) => {
+    if (message === "ok") {
+      alert("Your form submitted");
+    } else {
+      alert(message);
+    }
+  });
 }
